187d4205df3ab7dab374b6ac9514b58c6fdf3d1f
viswasvarmaK <viswasvarmasatya@msitprogram.net>
2018-08-01 23:50:44 +0530

    Activity - 13m

---
+++ cspp1_practise/m3/iterate_even.py
@@ -0,0 +1,7 @@
+n=10
+i=2
+print("print",i)
+while i<9:
+	i=i+2
+	print("print",i)
+print("Goodbye!")	
---
+++ cspp1_practise/m3/sum_1_end.py
@@ -0,0 +1,7 @@
+n=int(input("Enter the number"))
+result=0
+i=1
+while i<n+1:
+	result=result+i
+	i=i+1
+print(result)	
