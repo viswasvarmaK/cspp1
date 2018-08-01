97907499e142c2203851db69325e278457566e93
viswasvarmaK <viswasvarmasatya@msitprogram.net>
2018-08-01 23:49:05 +0530

    3rd program compare_AB.py

---
+++ cspp1_practise/m3/compare_AB.py
@@ -0,0 +1,8 @@
+varA=int(input("Enter a number"))
+varB=int(input("Enter a number"))
+if varA==varB:
+	print("equal")
+if varA>varB:
+	print("varA is greater than varB")
+if varA<varB:
+	print("varA is smaller than varB")	
