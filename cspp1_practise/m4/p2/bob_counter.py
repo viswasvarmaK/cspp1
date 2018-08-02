1c503c0ddbb090cdd4612b188f17d533bb2b528e
viswasvarmasatya <viswasvarmasatya@msitprogram.net>
2018-08-02 22:15:38 +0530

    " -> 5 of 5 passed. pylint: 10.00/10 "

--- cspp1_practise/m4/p2/bob_counter.py
+++ cspp1_practise/m4/p2/bob_counter.py
@@ -1,14 +1,22 @@
-'''Assume s is a string of lower case characters.
-
-Write a program that prints the number of times the string 'bob' occurs in s. For example, if s = 'azcbobobegghakl', then your program should print
-
-Number of times bob occurs is: 2'''
+'''
+Author: VISWAS
+Date: 2-8-2018
+'''
 
 def main():
-	s = raw_input()
-	# the input string is in s
-	# remove pass and start your code here
-	pass
+    '''
+    Function checking presence of substring
+    '''
+    s_1 = input()
+    # the input string is in s
+    # remove pass and start your code here
+    end = len(s_1)
+    ctr = "bob"
+    stringcount = 0
+    for i in range(0, end):
+        if s_1[i:i+3] == ctr:
+            stringcount = stringcount + 1
+    print(stringcount)
 
-if __name__== "__main__":
-	main()
+if __name__ == "__main__":
+    main()
