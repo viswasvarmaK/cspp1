bcb6c07c423086c7e39b2f663c21b1fdeb14d8fc
viswasvarmasatya <viswasvarmasatya@msitprogram.net>
2018-08-02 22:43:51 +0530

    " -> 1 of 1 passed. pylint: 8.00/10 "

--- sample-assignment/sol.py
+++ sample-assignment/sol.py
@@ -1,9 +1,10 @@
-# Exercise: Assignment-1
-# Write a Python function, factorial(n), that takes in one number and
-# returns the factorial of given number.
-
-# This function takes in one number and returns one number.
-
+'''
+Exercise: Assignment-1
+Write a Python function, factorial(n), that takes in one number and
+returns the factorial of given number.
+This function takes in one number and returns one number.
+@author : viswas
+'''
 def main():
     data = input()
     print(int(data[0])+int(data[3]))
