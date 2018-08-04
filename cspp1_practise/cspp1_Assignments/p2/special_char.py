9a4393b27129ba74c516ac9a2b5d535c59c5ccfa
viswasvarmasatya <viswasvarmasatya@msitprogram.net>
2018-08-04 11:54:13 +0530

    " -> 0 of 7 passed. pylint: 10.00/10 "

--- cspp1_practise/cspp1_Assignments/p2/special_char.py
+++ cspp1_practise/cspp1_Assignments/p2/special_char.py
@@ -10,8 +10,9 @@ def main():
     Read string from the input, store it in variable str_input.
     '''
     str_input = input()
+    i_i = 0
     s_s = ''
-    for i_i in str_input:
+    while i_i < str_input:
         if i_i in '!@#$%^&*':
             i_i = " "
             s_s = s_s + i_i
