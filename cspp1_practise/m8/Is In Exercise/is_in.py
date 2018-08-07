72a282ef54b83f21772a3d97cbc12d783bf6d810
viswasvarmasatya <viswasvarmasatya@msitprogram.net>
2018-08-08 00:09:55 +0530

    is_in

--- cspp1_practise/m8/Is In Exercise/is_in.py
+++ cspp1_practise/m8/Is In Exercise/is_in.py
@@ -1,47 +1,44 @@
 '''
-# Exercise: Is In
-Author: Viswas
+Author: VISWAS
 Date: 7-8-2018
 '''
-def is_in(char, input_s):
-    global low
-    global high 
-    global middle
-    middle = (low + high)//2
-    if low == high:
-        return False
-    if input_s[middle] == char:
-        return True
-    if input_s[middle] > char:
-        high = middle-1
-        return is_in(char, input_s)
-    if input_s[middle] < char:
-        low = middle+1
-        return is_in(char, input_s)
+# Exercise: Is In
+def is_in(char, input_str):
     '''
     char: a single character
     aStr: an alphabetized string
-    
     returns: True if char is in aStr; False otherwise
     '''
-    # Your code here
-   
-
+    global low_val
+    global high_val 
+    global middle_val
+    middle_val = (low_val + high_val)//2
+    if low_val == high_val:
+        return False
+    if input_str[middle_val] == char:
+        return True
+    if input_str[middle_val] > char:
+        high_val = middle_val-1
+        return is_in(char, input_str)
+    if input_str[middle_val] < char:
+        low_val = middle_val+1
+        return is_in(char, input_str) 
 def main():
-    data = input()
+    '''
+    Main Function starts here
+    '''
     data_1 = input()
-    if data_1 == "":
+    data_2 = input()
+    if data_2 == "":
         return print("False")
     else:
-        string_sort = sorted(data_1)
+        string_sort = sorted(data_2)
         string_sort = ''.join(string_sort)
-        global low
-        global high 
-        global middle
-        low = 0
-        high = len(data_1)
-        print(is_in(data, string_sort))
-if __name__ == "__main__":
+        global low_val
+        global high_val 
+        global middle_val
+        low_val = 0
+        high_val = len(data_2)
+        print(is_in(data_1, string_sort))
+if __name__== "__main__":
     main()
-
-   
