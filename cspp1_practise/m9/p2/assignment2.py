0aa996730f8b780066436ddcf6bb67ccc784c5f6
viswasvarmasatya <viswasvarmasatya@msitprogram.net>
2018-08-08 21:04:07 +0530

    " -> 9 of 9 passed. pylint: 10.00/10 "

--- cspp1_practise/m9/p2/assignment2.py
+++ cspp1_practise/m9/p2/assignment2.py
@@ -1,9 +1,7 @@
 '''
 Exercise: Assignment-2
-Next, implement the function getGuessedWord that takes in two parameters
-a string, secret_word, and a list of letters, letters_guessed. This function
-returns a string that is comprised of letters and underscores, based on what
-letters in letters_guessed are in secret_word. This shouldn't be too different from isWordGuessed!
+Author: Viswas
+Date: 8-8-2018
 '''
 def get_guessed_word(secret_word, letters_guessed):
     '''
