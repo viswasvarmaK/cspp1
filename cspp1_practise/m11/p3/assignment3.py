8e36fccc329380a4c6daa3221938676d86fd143e
viswasvarmasatya <viswasvarmasatya@msitprogram.net>
2018-08-10 22:55:22 +0530

    " -> 2 of 3 passed. pylint: 9.58/10 "

--- cspp1_practise/m11/p3/assignment3.py
+++ cspp1_practise/m11/p3/assignment3.py
@@ -5,13 +5,11 @@ Date : 10-8-2018
 
 '''
 
-def isValidWord(word, hand, word_list):
+def is_valid_word(word, hand, word_list):
     """
     Returns True if word is in the wordList and is entirely
-    composed of letters in the hand. Otherwise, returns False.
-
+    composed of letters in the hand. Otherwise, returns False
     Does not mutate hand or wordList.
-   
     word: string
     hand: dictionary (string -> int)
     wordList: list of lowercase strings
@@ -23,22 +21,23 @@ def isValidWord(word, hand, word_list):
         if i in hand:
             count += 1
     if count == len_1:
-        for char in word_list:
+        for i in len(word_list):
             if word in word_list:
                 return True
     return False
 def main():
-    word=input()
-    n=int(input())
-    adict={}
-    for i in range(n):
-        data=input()
-        l=data.split()
-        adict[l[0]]=int(l[1])
-    l2=input().split()
-    print(isValidWord(word,adict,l2))
-        
-
-
-if __name__== "__main__":
+    """
+    main function
+    """
+    word = input()
+    n_n = int(input())
+    adict = {}
+    for i in range(n_n):
+        i += 1#for pilint
+        data = input()
+        l_1 = data.split()
+        adict[l_1[0]] = int(l_1[1])
+    l_2 = input().split()
+    print(is_valid_word(word, adict,l_2))
+if __name__ == "__main__":
     main()
