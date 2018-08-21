713fe9e0248eef7dbcdc4c80282ee6aec0fa9587
viswasvarmasatya <viswasvarmasatya@msitprogram.net>
2018-08-22 01:18:05 +0530

    " -> 5 of 5 passed. pylint: 9.80/10 "

--- cspp1_practise/m14/p2/assignment2.py
+++ cspp1_practise/m14/p2/assignment2.py
@@ -1,34 +1,16 @@
-''' Problem 2 - PlaintextMessage '''
-# For this problem, the graders will use our implementation of the Message class,
-# so don't worry if you did not get the previous parts correct.
-
-# PlaintextMessage is a subclass of Message and has methods to encode a string
-# using a specified shift value. Our class will always create an encoded version
-# of the message, and will have methods for changing the encoding.
-
-# Implement the methods in the class PlaintextMessage according to the specifications in ps6.py.
-# The methods you should fill in are:
-# __init__(self, text, shift): Use the parent class constructor to make your code more concise.
-# The getter method get_shift(self)
-# The getter method get_encrypting_dict(self): This should return a COPY of self.encrypting_dict.
-# The getter method get_message_text_encrypted(self)
-# change_shift(self, shift): Think about what other methods you can use to make this easier.
-
-### Helper code
-
+'''
+Author: Viswas
+Date : 21-8-2018
+'''
 import string
-
 def load_words(file_name):
     '''
     file_name (string): the name of the file containing
     the list of words to load
-
     Returns: a list of valid words. Words are strings of lowercase letters.
-
     Depending on the size of the word list, this function may
     take a while to finish.
     '''
-
     # inFile: file
     in_file = open(file_name, 'r')
     # line: string
@@ -47,9 +29,7 @@ class Message(object):
     def __init__(self, text):
         '''
         Initializes a Message object
-
         text (string): the message's text
-
         a Message object has two attributes:
             self.message_text (string, determined by input text)
             self.valid_words (list, determined using helper function load_words
@@ -126,25 +106,17 @@ class Message(object):
                 new_msg.append(self.build_shift_dict(shift)[i])
         return ''.join(new_msg)
 
-### Helper code End
-
-
-
-### Paste your implementation of the `PlaintextMessage` class here
-
-
-
 def main():
     ''' Function to handle testcases '''
     inp = input()
-    data = PlaintextMessage(inp, int(input()))
-    print(data.get_shift())
-    print(data.get_encrypting_dict())
-    print(data.get_message_text_encrypted())
-    data.change_shift(int(input()))
-    print(data.get_shift())
-    print(data.get_encrypting_dict())
-    print(data.get_message_text_encrypted())
-
+    data = Message(inp)
+    shift = int(input())
+    print(shift)
+    print(data.build_shift_dict(shift))
+    print(data.apply_shift(shift))
+    shift = int(input())
+    print(shift)
+    print(data.build_shift_dict(shift))
+    print(data.apply_shift(shift))
 if __name__ == "__main__":
     main()
