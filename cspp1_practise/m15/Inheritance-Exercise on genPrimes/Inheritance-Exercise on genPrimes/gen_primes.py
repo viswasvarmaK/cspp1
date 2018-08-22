1ebac8203b95021a73abe22658cd3a14a4b43818
viswasvarmasatya <viswasvarmasatya@msitprogram.net>
2018-08-22 23:42:03 +0530

    m15

--- cspp1_practise/m15/Inheritance-Exercise on genPrimes/Inheritance-Exercise on genPrimes/gen_primes.py
+++ cspp1_practise/m15/Inheritance-Exercise on genPrimes/Inheritance-Exercise on genPrimes/gen_primes.py
@@ -1,17 +1,28 @@
 #define the gen_primes function here
-def genPrimes():
-    pass
+def genPrimes(a):
+	i = 2
+	count = 0
+	while count < a:
+		f = 0
+		for j in range(1, i):
+			if i%j == 0:
+				f += 1
+		if f == 1:
+			yield i
+			count += 1
+		i += 1    	
+    
 
 def main():
-	data=raw_input()
+	data=input()
 	l=data.split()
 	a=int(l[0])
 	b=int(l[1])
-	primeGenerator = genPrimes()
+	primeGenerator = genPrimes(a)
 	for i in range(a):
-	    p=primeGenerator.next()
+	    p=primeGenerator.__next__()
 	    if(i%b==0):
-	        print p
+	        print(p)
 	
 if __name__== "__main__":
 	main()
