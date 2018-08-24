8b6326448eaaf35af2b222d76c842e797357d40c
viswasvarmasatya <viswasvarmasatya@msitprogram.net>
2018-08-24 11:20:28 +0530

    " -> 2 of 2 passed. pylint: 8.82/10 "

--- cspp1_practise/m12/p2/social_network.py
+++ cspp1_practise/m12/p2/social_network.py
@@ -50,8 +50,8 @@ def delete_person(network, arg1):
     for i in network:
         if arg1 in network[i]:
             network[i].remove(arg1)
-    if arg2 in network:
-        del network[arg2]
+    if arg1 in network:
+        del network[arg1]
     return network            
 
 def main():
