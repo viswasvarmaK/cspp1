7dc7099b3f668caf900464d995e790f2df510acb
viswasvarmasatya <viswasvarmasatya@msitprogram.net>
2018-08-25 11:42:59 +0530

    " -> 0 of 3 passed. pylint: 8.06/10 "

--- cspp1_practise/m22/assignment2/assignment2/clean_input.py
+++ cspp1_practise/m22/assignment2/assignment2/clean_input.py
@@ -7,27 +7,27 @@ def clean_string(string):
     str_1 = string
     for i in str_1:
         if i == '!':
-            str_1.translate('', '!')   
+            str_1.translate(None, '!')   
         if i == '@':
-            str_1.translate('', '@')
+            str_1.translate(None, '@')
         if i == '#':
-            str_1.translate('', '#')
+            str_1.translate(None, '#')
         if i == '$':
-            str_1.translate('', '$')
+            str_1.translate(None, '$')
         if i == '%':
-            str_1.translate('', '%')
+            str_1.translate(None, '%')
         if i == '^':
-            str_1.translate('', '^')
+            str_1.translate(None, '^')
         if i == '&':
-            str_1.translate('', '&')
+            str_1.translate(None, '&')
         if i == '*':
-            str_1.translate('', '*')
+            str_1.translate(None, '*')
         if i == '(':
-            str_1.translate('', '(')
+            str_1.translate(None, '(')
         if i == ')':
-            str_1.translate('', ')')
+            str_1.translate(None, ')')
         if i == ' ':
-            str_1.translate('', ' ')
+            str_1.translate(None, ' ')
 
     return str_1
     # for char in string:
