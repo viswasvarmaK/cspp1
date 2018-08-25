3aa0a582f37a038b7befffe13201be16fc90dd92
viswasvarmasatya <viswasvarmasatya@msitprogram.net>
2018-08-25 10:43:02 +0530

    " -> 0 of 2 passed. pylint: 0/0 "

--- cspp1_practise/m22/assignment1/assignment1/read_input.py
+++ cspp1_practise/m22/assignment1/assignment1/read_input.py
@@ -6,7 +6,7 @@ def main():
 	no_of_lines = input().split(" ")
 	lines = ""
 	for i in range(no_of_lines):
-		lines += input() + "\n"
+		lines += no_of_lines[i] + "\n"
     print(lines)
 
 if __name__ == '__main__':
