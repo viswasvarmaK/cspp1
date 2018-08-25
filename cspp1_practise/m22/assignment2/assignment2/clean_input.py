fdd09de439195fd427dd35183594faf8e78553df
viswasvarmasatya <viswasvarmasatya@msitprogram.net>
2018-08-25 10:57:52 +0530

    " -> 0 of 3 passed. pylint: 0/0 "

--- cspp1_practise/m22/assignment2/assignment2/clean_input.py
+++ cspp1_practise/m22/assignment2/assignment2/clean_input.py
@@ -4,6 +4,7 @@ alphabets in both upper and lower case and numbers.
 '''
 
 def clean_string(string):
+	string = list(string)
     for char in range(string):
     	if char in "!@#$%^&*()":
     	    char = ""
